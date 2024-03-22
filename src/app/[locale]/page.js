646ecb53342a9faf8b8e332001a89189489d77b6
Sandeep <sandbro7163337@gmail.com>
2024-03-22 12:35:16 +0530

    add video

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -98,7 +98,11 @@ const Home = () => {
   return (
     <div className={`w-full h-full absolute   z-[-1] ${isScroll?'top-36':''}  `}>
       {/* HOME CARAOUSAL IMAGE */}
-      <video src='/video/banner.mov'     autoPlay loop playsInline className={`w-full  object-fill ${isScroll?'h-[700px]':'h-screen'}  `}/>
+      <video   autoPlay loop   className={`w-full  object-fill ${isScroll?'h-[700px]':'h-screen'}  `}>
+        <source src="/video/banner.mov"   />
+        Your browser does not support the video tag.
+      </video>
+      {/* <video src='/video/banner.mov'     autoPlay loop playsInline className={`w-full  object-fill ${isScroll?'h-[700px]':'h-screen'}  `}/> */}
       {/* <CarouselImage images={banners || []}  /> */}
 
       <section className="w-full    pt-20  relative z-[1] bg-primary-subtle ">
