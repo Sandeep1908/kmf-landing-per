4e19d97a7f6076635cb1d22887ed266b29751a4d
Aditya <aadimalviyaa@gmail.com>
2024-07-05 13:00:18 +0530

    add Head tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,9 @@ import { Header } from '../components/Header'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './page.module.css'
 import logo from '@/images/logo/logo.png';
- 
+import Head from 'next/head';
+
+
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -13,13 +15,14 @@ import "swiper/css/pagination";
 import 'rodal/lib/rodal.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
- 
+
 import { register } from 'swiper/element/bundle';
 import { ParallaxProvider } from "react-scroll-parallax";
 import { pdfjs } from 'react-pdf';
 
 import 'react-photo-view/dist/react-photo-view.css';
 import { MyContextProvider } from '@/context/headerContext';
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -28,34 +31,47 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 register();
 
 export default function RootLayout({ children }) {
-   
-  
- 
-   
+
+
+
+
   return (
 
     <>
       <html lang="en">
         <head>
           <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet" />
-          <link rel="icon" type="image/png" href={logo.src}/>
+          <link rel="icon" type="image/png" href={logo.src} />
         </head>
+        <Head>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-DP4FBEQWDL"></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DP4FBEQWDL');
+            `,
+              }}
+            />
+          </Head>
         <body className='relative'>
           <MyContextProvider>
-          <Header />
-          <ParallaxProvider>
-          {children}
-          <SpeedInsights />
-          </ParallaxProvider>
+            <Header />
+            <ParallaxProvider>
+              {children}
+              <SpeedInsights />
+            </ParallaxProvider>
           </MyContextProvider>
           {''}
-       
 
-       
+
+
         </body>
-        
+
       </html>
-     
+
     </>
   );
 }
