c292a947b6e36bf71e292147e2cd1ae60870284b
Aditya <aadimalviyaa@gmail.com>
2024-07-24 15:52:21 +0530

    Fix magazine section

--- src/app/[locale]/portfolio/PdfPreview.js
+++ src/app/[locale]/portfolio/PdfPreview.js
@@ -17,7 +17,7 @@ function PdfPreview({ pdfUrl,count }) {
   return (
     <div className="relative pdf-preview-container z-[1] group  ">
           <div className='w-full'> <h1 className=' pl-[50%] md:pl-[40%] mb-5'>{count}</h1></div>
-         <div className='w-[400px] h-[530px] absolute z-[5]   '>
+         <div className='w-[340px] lg:w-[400px] h-[530px] absolute z-[5]   '>
           <div className='absolute w-full h-full bg-black z-[10] opacity-45 hidden group-hover:block'>  </div>
 
           <button className="absolute top-[45%] left-[35%] z-[100] text-sm  cursor-pointer bg-primary-gradient p-2 text-white hidden group-hover:block  " onClick={() => window.open(pdfUrl, '_blank')}>View Fullscreen</button>
