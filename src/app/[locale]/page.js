d1d39c9ba6a9501a2b94f1c6db5dc1b6dfe3d7ca
Sandeep <sandbro7163337@gmail.com>
2024-08-08 11:18:01 +0530

    add controls

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -180,7 +180,7 @@ const Home = () => {
       <video
         className={`w-full object-fill  ${isScroll ? 'h-[200px] md:h-[700px]' : ''}`}
         src="/video/bannernewvid.mov"
-         
+        controls
         muted
         autoPlay
         loop
