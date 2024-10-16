136090728527c08153c2f92470bab6cf91b3b9a1
Sandeep <sandbro7163337@gmail.com>
2024-10-16 17:58:39 +0530

    fix again secret info

--- src/app/[locale]/secret-info/page.js
+++ src/app/[locale]/secret-info/page.js
@@ -1,28 +1,19 @@
-import React from 'react'
-import { Header } from '@/components/Header'
+import React from 'react';
+import { Header } from '@/components/Header';
 
 function SecretInfo() {
   return (
-
-    <div className='w-full h-full'>
-      
-      
-      <div className='w-full  relative'>
-
-  <iframe allowfullscreen="allowfullscreen" scrolling="no" className="fp-iframe object-contain" src="https://heyzine.com/flip-book/a4d70474a9.html" style={{border: '1px solid lightgray' ,width: '100%' ,height: '100vh'}} ></iframe>
-
-  
-    
-    
-      
-
+    <div className="w-full h-full">
+      <div className="w-full  relative">
+        <iframe
+          allowfullscreen="allowfullscreen"
+          scrolling="no"
+          className="fp-iframe object-contain"
+          src="https://heyzine.com/flip-book/a4d70474a9.html"
+          style={{ border: '1px solid lightgray', width: '100%', height: '100vh' }}></iframe>
       </div>
-
-    
-      </div>
-   
- 
-  )
+    </div>
+  );
 }
 
-export default SecretInfo
+export default SecretInfo;
