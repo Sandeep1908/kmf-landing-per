1eb01f065ea5f145c9831077e5be88ca0cd7a45c
Sandeep <sandbro7163337@gmail.com>
2024-10-16 17:46:57 +0530

    add secret info

---
+++ src/app/[locale]/secret-info/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Header } from '@/components/Header'
+
+function SecretInfo() {
+  return (
+
+    <div className='w-full h-full'>
+      
+      <div className='w-full h-[98vh] flex justify-center items-center'>
+
+      <iframe allowfullscreen="" scrolling="no" className="fp-iframe"    style={{border: '1px solid lightgray', width: '100%', height: '381px'}} src="https://heyzine.com/flip-book/a4d70474a9.html"></iframe>
+      </div>
+   
+    </div>
+  )
+}
+
+export default SecretInfo
