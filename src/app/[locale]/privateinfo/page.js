710156eca86422ec0b319200ca1cb9be21f3b50a
Wrathdemon <57354589+prabhatjoshi321@users.noreply.github.com>
2025-03-16 20:37:53 +0530

    Update page.js
    Changes to privateinfo

--- src/app/[locale]/privateinfo/page.js
+++ src/app/[locale]/privateinfo/page.js
@@ -11,17 +11,27 @@ function PrivateInfo() {
             <div className='flex flex-col space-y-3 border p-2 '>
                   <div className='flex flex-col'>
                       <p className='text-lg font-[400]'>Manufactured & Packed By:</p>
-                      <h1 className='text-xl font-semibold'>GovindJee Dairy Milk Pvt Ltd</h1>
-                      <p className='text-lg font-[400]'>NH-11, Village:Dhodhsar, Sikar Road,
-                      Tehsil:Chomu, District:Jaipur, Rajasthan - 303712.
+                      <h1 className='text-xl font-semibold'>GM - Govindjee Dairy Milk Private Limited</h1>
+                      <p className='text-lg font-[400]'>NH- 11, Village: Dhodhsar, Sikar Road, Tehsil
+                        Chomu, District: Jaipur, Rajasthan – 303 712.
                       </p>
                       <h1 className='text-lg font-semibold'>Lic.No. 10012013000295</h1>
                     </div>
 
+                    <div className='flex flex-col'>
+                      <p className='text-lg font-[400]'>Manufactured & Packed By:</p>
+                      <h1 className='text-xl font-semibold'>SN - S.N Dairy Foods Private Limited</h1>
+                      <p className='text-lg font-[400]'>Ajroi Road, Sasni, Hathras Distt Sasni Tehsil,
+                        Uttarpradesh – 204 216.
+                      </p>
+                      <h1 className='text-lg font-semibold'>Lic.No. 10014051001255</h1>
+                    </div>
+
                     <div>
                     
-                      <h1 className='text-lg font-semibold'>For: Mandya Co-operative Milk Union Limited</h1>
-                      <p className='text-xl font-semibold'>Product Dairy, Gejjalagere – 571 428, Mandya, Karnataka
+                      <h1 className='text-lg font-semibold'>For: Mandya Co-operative Milk Union Limited.</h1>
+                      <p className='text-xl font-semibold'>Product Dairy, Gejjalagere – 571 428,
+                        Mandya, Karnataka
                       </p>
                       <h1 className='text-xl font-semibold'>Lic. No. 10012043000208</h1>
 
